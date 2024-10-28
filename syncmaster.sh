f25b748719983f763f2c2c55111ece3bae955686
Tom Huff <thuff@nvidia.com>
2024-10-27 18:56:08 -0700

    tweaks on repo sync

--- syncmaster.sh
+++ syncmaster.sh
@@ -3,10 +3,11 @@
 # push the devbranch to dev repo
 # synch the repos
 #
-if [ $# -lt ${1} ];then
-  echo "usages:$0 {devbranch}"
+if [ $# -lt 2 ];then
+  echo "usages:$0 {devbranch} {commit comment}"
   exit 1
 fi
+git commit -m "${2}"
 ./gitpushrepo.sh dev ${1}
 ./syncrepo.sh dev origin master
 git push
