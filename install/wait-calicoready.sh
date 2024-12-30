8dc227680c894426bf02f4fbb2a5a2f73814a3dc
Tom Huff <thuff@nvidia.com>
2024-12-30 11:05:48 -0800

    adjust calico start wait

--- install/wait-calicoready.sh
+++ install/wait-calicoready.sh
@@ -2,5 +2,5 @@
 #
 #
 source ${NETOP_ROOT_DIR}/install/readytest.sh
-PODLIST=( 2,calico-apiserver,calico-apiserver 1,calico-system,calico-kube-controllers 1,calico-system,calico-node 1,calico-system,calico-typha 1,calico-system,csi-node-driver 2,kube-system,coredns 1,tigera-operator,tigera-operator 2,kube-system,coredns )
+PODLIST=( 2,calico-apiserver,calico-apiserver 1,calico-system,calico-kube-controllers 1,calico-system,calico-node 1,calico-system,calico-typha 1,calico-system,csi-node-driver 1,kube-system,kube-apiserver 1,tigera-operator,tigera-operator 1,kube-system,kube-scheduler )
 nsReady
