65d913507fce638a74f0ce27860a14902c0f068e
Aleksey Senin <alekseys@nvidia.com>
2025-02-14 11:37:12 -0500

    Use full path to values.yaml for helm install
    Also dropping pushd/popd

--- install/ins-network-operator.sh
+++ install/ins-network-operator.sh
@@ -12,13 +12,13 @@ if [ "${X}" = "0" ];then
   kubectl create ns ${NETOP_NAMESPACE}
 fi
 ${NETOP_ROOT_DIR}/install/mksecret.sh
-pushd .
+
 cd ${USECASE_DIR}
 ${NETOP_ROOT_DIR}/ops/mk-values.sh
 ${NETOP_ROOT_DIR}/ops/mk-nic-cluster-policy.sh
 ${NETOP_ROOT_DIR}/ops/mk-network-cr.sh
-popd
-cd ${NETOP_ROOT_DIR}/release/${NETOP_VERSION}/netop-chart/network-operator
-#CHART_VALUES="${NETOP_ROOT_DIR}/release/${NETOP_VERSION}/netop-chart/network-operator/values.yaml"
-helm install -n ${NETOP_NAMESPACE} network-operator nvidia/network-operator --version ${NETOP_VERSION} -f ./values.yaml -f ${USECASE_DIR}/values.yaml
-${NETOP_ROOT_DIR}/install/applycrds.sh 
+
+helm install -n ${NETOP_NAMESPACE} network-operator nvidia/network-operator --version ${NETOP_VERSION} \
+  -f ${NETOP_ROOT_DIR}/release/${NETOP_VERSION}/netop-chart/network-operator/values.yaml \
+  -f ${USECASE_DIR}/values.yaml
+${NETOP_ROOT_DIR}/install/applycrds.sh
