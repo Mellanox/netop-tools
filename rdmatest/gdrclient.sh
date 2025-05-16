030a4fbface54d7e159f603a97410a0d3eaf70d3
Tom Huff <thuff@nvidia.com>
2025-05-16 11:03:56 -0700

    tests using show_gids

--- rdmatest/gdrclient.sh
+++ rdmatest/gdrclient.sh
@@ -2,23 +2,19 @@
 #
 # ${1}=client pod
 # ${2}=target server pod
-# ${3}=net_dev (net1,net2,net3,net4,...)
 #
-if [ $# -lt 3 ];then
-  echo "usage:${0} <client_pod> <server_pod> <net_dev> [--gdr]"
+if [ $# -lt 2 ];then
+  echo "usage:${0} <client_pod> <server_pod> [--gdr]"
   exit 1
 fi
 source ${NETOP_ROOT_DIR}/global_ops.cfg
-CLIENT_POD=${1}
-shift
-SERVER_POD=${1}
-shift
-NET_DEV=${1}
-shift
 
-RDMA_DEV=`${K8CL} exec ${CLIENT_POD} -- sh -c "rdma link | grep ${NET_DEV} | cut -d' ' -f2 | cut -d'/' -f1"`
-IP=`${K8CL} exec ${SERVER_POD} -- sh -c "ip -br a show ${NET_DEV}" | tr -s [:space:] | cut -d' ' -f3 | cut -d'/' -f1`
-echo RDMA DEV = ${RDMA_DEV}
+CLIENT_POD=${1}
+SERVER_POD=${2}
+NET_DEV=net1
+RDMA_DEV=`${K8CL} exec ${CLIENT_POD} -- sh -c "./show_gids | grep -i ${NET_DEV} | grep v2 | grep -v fe80 | cut -f1"`
+GID_IDX=`${K8CL} exec ${CLIENT_POD} -- sh -c "./show_gids | grep -i ${NET_DEV} | grep v2 | grep -v fe80 | cut -f3"`
+IP=`${K8CL} exec ${SERVER_POD} -- sh -c "./show_gids | grep -i ${NET_DEV} | grep v2 | grep -v fe80 | cut -f5"`
 
 GDR=false
 for arg in "$@"; do
@@ -34,13 +30,14 @@ done
 
 if [ "${GDR}" == false ];then
   echo "--gdr flag not provided. Performing rdma perftest."
-  ${K8CL} exec ${CLIENT_POD} -- bash -c "ib_write_bw -D 5 -d ${DEV} -F --report_gbits  -p 123  ${IP}"
+  echo "ib_write_bw -d ${RDMA_DEV} -F -x ${GID_IDX} --report_gbits -p 123 -a ${IP}"
+  ${K8CL} exec ${CLIENT_POD} -- bash -c "ib_write_bw -d ${RDMA_DEV} -F -x ${GID_IDX} --report_gbits  -p 123 -a ${IP}"
 fi
 
 if [ "${GDR}" == true ];then
   echo "--gdr flag Provided. Determining optimal CUDA device. This may take a few seconds ..."
-  CUDA_DEV=`${K8CL} exec ${CLIENT_POD} -- bash -c "./k8s-netdev-mapping.sh | grep ${NET_DEV} | tail -1 | cut -d ' ' -f7"`
-  BEST_GPU_LINK=`${K8CL} exec ${CLIENT_POD} -- bash -c "./k8s-netdev-mapping.sh | grep ${NET_DEV} | tail -2 | head -1 | cut -d ' ' -f6"`
+  CUDA_DEV=`${K8CL} exec ${SERVER_POD} -- bash -c "./k8s-netdev-mapping.sh | grep ${NET_DEV} | cut -f 6"`
+  BEST_GPU_LINK=`${K8CL} exec ${SERVER_POD} -- bash -c "./k8s-netdev-mapping.sh | grep ${NET_DEV} | cut -f 5"`
   echo "Using CUDA device ${CUDA_DEV} via ${BEST_GPU_LINK}. Performing GDR perftest."
-  ${K8CL} exec ${CLIENT_POD} -- bash -c "ib_write_bw -D 5 -d ${RDMA_DEV} -F --report_gbits -p 123 --use_cuda=${CUDA_DEV} ${IP}"
+  ${K8CL} exec ${CLIENT_POD} -- bash -c "ib_write_bw -d ${RDMA_DEV} -F -x ${GID_IDX} --report_gbits -p 123 --use_cuda=${CUDA_DEV} -a ${IP}"
 fi
