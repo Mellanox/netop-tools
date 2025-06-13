72077d4cda6130ade255228958c7771fbeebabd3
Tom Huff <thuff@nvidia.com>
2025-06-13 09:00:44 -0700

    move temp data files to /tmp, cleanup after execution

--- rdmatest/gdrclient.sh
+++ rdmatest/gdrclient.sh
@@ -32,13 +32,16 @@ for arg in "$@"; do
     # Add more flags here as needed
   esac
 done
+GID_INFO_FILE_SRVR="/tmp/gid_info_srvr.$$"
+GID_INFO_FILE_CLNT="/tmp/gid_info_clnt.$$"
+CUDA_INFO_FILE="/tmp/cuda_info.$$"
 
-${K8CL} exec ${CLNT_POD} -- sh -c "/root/show_gids" > gid_info_clnt.$$
-GID_INFO=$(gid_info ${NET_DEV} ./gid_info_clnt.$$)
+${K8CL} exec ${CLNT_POD} -- sh -c "/root/show_gids" > ${GID_INFO_FILE_CLNT}
+GID_INFO=$(gid_info ${NET_DEV} ${GID_INFO_FILE_CLNT})
 RDMA_DEV=$(echo $GID_INFO |cut -d' ' -f1)
 GID_IDX=$(echo $GID_INFO |cut -d' ' -f3)
-${K8CL} exec ${SRVR_POD} -- sh -c "/root/show_gids" > gid_info_srvr.$$
-GID_INFO=$(gid_info ${NET_DEV} gid_info_srvr.$$)
+${K8CL} exec ${SRVR_POD} -- sh -c "/root/show_gids" > ${GID_INFO_FILE_SRVR}
+GID_INFO=$(gid_info ${NET_DEV} ${GID_INFO_FILE_SRVR})
 IP=$(echo $GID_INFO |cut -d' ' -f5)
 
 if [ "${GDR}" == false ];then
@@ -49,10 +52,10 @@ fi
 
 if [ "${GDR}" == true ];then
   echo "--gdr flag Provided. Determining optimal CUDA device. This may take a few seconds ..."
-  ${K8CL} exec ${SRVR_POD} -- bash -c "/root/k8s-netdev-mapping.sh" > cuda_info.$$
-  CUDA_DEV=$(grep ${NET_DEV}, cuda_info.$$| cut  -d',' -f6)
-  BEST_GPU_LINK=$(grep ${NET_DEV}, cuda_info.$$| cut  -d',' -f5)
+  ${K8CL} exec ${SRVR_POD} -- bash -c "/root/k8s-netdev-mapping.sh" > ${CUDA_INFO_FILE}
+  CUDA_DEV=$(grep ${NET_DEV}, ${CUDA_INFO_FILE}| cut  -d',' -f6)
+  BEST_GPU_LINK=$(grep ${NET_DEV}, ${CUDA_INFO_FILE}| cut  -d',' -f5)
   echo "Using CUDA device ${CUDA_DEV} via ${BEST_GPU_LINK}. Performing GDR perftest."
   ${K8CL} exec ${CLNT_POD} -- bash -c "ib_write_bw -d ${RDMA_DEV} -F -x ${GID_IDX} --report_gbits -p 123 --use_cuda=${CUDA_DEV} -a ${IP}"
 fi
-rm -f gid_info_clnt.$$ gid_info_srvr.$$
+rm -f ${GID_INFO_FILE_CLNT} ${GID_INFO_FILE_SRVR} ${CUDA_INFO_FILE}
