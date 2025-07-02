8179b8518a83ee5f8181439a6548fb5ea40f20f8
Tom Huff <thuff@nvidia.com>
2025-07-02 09:04:32 -0700

    make arp settings through a pod env variable

--- rdmatools/entrypoint.sh
+++ rdmatools/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo "SYSCTL_CONFIG:${SYSCTL_CONFIG}"
+IFS=',' read SYSVALS <<< ${SYSCTL_CONFIG}
+for SYSVAL in ${SYSVALS};do
+  sysctl ${SYSVAL}
+done
 echo "NVIDIA rdmadbg_cuda container sleeping"
 sleep infinity & wait
-
