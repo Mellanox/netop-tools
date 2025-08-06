e2762966682ee3b583518b2119fda2ba13b29112
thuff <thuff@nvidia.com>
2025-08-06 15:05:15 -0700

    mft has to be installed along with kernel module builds

--- rdmatools/getpkgs.sh
+++ rdmatools/getpkgs.sh
@@ -6,4 +6,5 @@ apt-get update
 apt-get install -y git libtool autotools-dev automake autoconf m4 flex bison gawk
 apt-get install -y libibverbs-dev librdmacm-dev vim
 apt-get install -y libcap2-bin infiniband-diags iproute2 ibverbs-utils iputils-ping
-apt-get install -y net-tools mft traceroute dnsutils tcpdump kmod
+apt-get install -y net-tools traceroute dnsutils tcpdump kmod
+# mft
