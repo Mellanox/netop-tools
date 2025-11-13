9c84c6274e796909f9a59f1b194837db857a2c00
thuff <thuff@nvidia.com>
2025-11-13 09:41:33 -0800

    added more dhcp reset notes

--- ops/vmclone.sh
+++ ops/vmclone.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #
 #
+cat << HEREDOC
+in /etc/netplan/50-cloud-init.yaml
+under the option
+  dhcp4: true
+add the option
+  dhcp-identifier: mac
+to force MAC based DHCP assignment
+HEREDOC
 rm -f /etc/machine-id
 systemd-machine-id-setup
 reboot
