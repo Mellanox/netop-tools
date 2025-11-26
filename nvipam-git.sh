9f1ec29e7931212fbb07d02b000ed70d8313a199
thuff <thuff@nvidia.com>
2025-11-26 11:50:15 -0800

    make sure clones parallel to netop-tools repo

--- nvipam-git.sh
+++ nvipam-git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd ..
 git clone https://github.com/Mellanox/nvidia-k8s-ipam.git
 cd nvidia-k8s-ipam
 git checkout ${1}
 cd deploy/crds
-kubectl apply -f nv-ipam.nvidia.com_cidrpools.yaml -f nv-ipam.nvidia.com_ippools.yaml
+kubectl apply -f nv-ipam.nvidia.com_cidrpools.yaml,nv-ipam.nvidia.com_ippools.yaml
