da5a4c0af9755bed31f3b5bcee4975b9cba86acb
thuff <thuff@nvidia.com>
2025-11-26 11:44:16 -0800

    fix paths

--- nicconfig-ops-git.sh
+++ nicconfig-ops-git.sh
@@ -4,4 +4,8 @@
 git clone https://github.com/Mellanox/nic-configuration-operator.git
 cd ./nic-configuration-operator
 git checkout ${1}
-apply -f ./config/crd/bases/nicconfigurationtemplate.nvidia.com_nicconfigurationtemplates.yaml .
+cd deployment/nic-configuration-operator-chart/crds
+FILES=$(ls *.yaml)
+for FILE=${FILES[@]};do
+  apply -f ${FILE}
+done
