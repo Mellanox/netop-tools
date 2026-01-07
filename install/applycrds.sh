22ba97915dabf569f90a16620de52f1016f1d6f7
Aleksey Senin <alekseys@nvidia.com>
2026-01-07 14:01:40 -0500

    Add installation of missing CRD
    Adding sriov-network-operator  CRD

--- install/applycrds.sh
+++ install/applycrds.sh
@@ -6,8 +6,14 @@ source ${NETOP_ROOT_DIR}/global_ops.cfg
 DIRCRD="${NETOP_ROOT_DIR}/release/${NETOP_VERSION}/netop-chart/network-operator"
 ${docmd} ${K8CL} apply -f ${DIRCRD}/crds
 case "${USECASE}" in
-sriovnet_rdma|sriovib_net)
-  ${docmd} ${K8CL} apply -f ${DIRCRD}/charts/sriov-network-operator/crds
+sriovnet_rdma|sriovibnet_rdma)
+  case ${NETOP_VERSION} in
+  25.10.*)
+    ${docmd} ${K8CL} apply -f ${DIRCRD}/charts/sriov-network-operator/crds
+     ;;
+  *)
+     ;;
+  esac
 esac
 if [ "${NIC_CONFIG_ENABLE}" = "true" ];then
   ${docmd} ${K8CL} apply -f ${DIRCRD}/charts/nic-configuration-operator-chart/crds
