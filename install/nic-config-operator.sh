d49475039f4cc1f4e7256e2d52c68905652fffca
thuff <thuff@nvidia.com>
2026-07-09 15:49:50 -0700

    use

--- install/nic-config-operator.sh
+++ install/nic-config-operator.sh
@@ -8,5 +8,5 @@ cd /tmp/nic-configuration-operator
 # Use the tag matching your Network Operator release if present
 git checkout network-operator-v${NETOP_VERSION}
 
-kubectl apply -f deployment/nic-configuration-operator-chart/crds
-kubectl wait --for=condition=Established crd/nicconfigurationtemplates.configuration.net.nvidia.com --timeout=60s
+${K8CL} apply -f deployment/nic-configuration-operator-chart/crds
+${K8CL} wait --for=condition=Established crd/nicconfigurationtemplates.configuration.net.nvidia.com --timeout=60s
